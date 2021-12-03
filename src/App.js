6d1940829c5a629efd6be3c80c0cc81cde6d53b6
haelina <56834622+haelina@users.noreply.github.com>
2021-12-03 16:52:35 +0200

    Modify route names and app structure

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
+import { Home } from "./Components/Pages/Home";
+import { Projects } from "./Components/Pages/Projects";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      <h1>CV Hanna Sepänmaa</h1>
+      <div className="ButtonBar">
+        <button>Etusivu</button>
+        <button>CV</button>
+        <button>Projects</button>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Routes>
     </div>
-  );
-}
-
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
   );
 }
 
