5a88f9a5abeb0a89b1cd5a9d890b176bd9b864c3
haelina <56834622+haelina@users.noreply.github.com>
2022-01-06 17:40:27 +0200

    Start working with the projectcards

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,11 +1,16 @@
-import * as React from "react";
+import React from "react";
+import { ProjectCard } from "../organisms/ProjectCard";
 
 export const Projects = () => {
+  const projects = [{ title: "esim1" }];
   return (
     <>
       <main>
         <h2>PROJEKTIT</h2>
         <p>That feels like an existential question, don't you think?</p>
+        {projects.map((p) => {
+          return <ProjectCard title={p.title}></ProjectCard>;
+        })}
       </main>
     </>
   );
---
+++ src/Components/organisms/ProjectCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ProjectCard = ({ title, img, description }) => {
+  return (
+    <div className="projectcard">
+      <h2>{title}</h2>
+    </div>
+  );
+};
