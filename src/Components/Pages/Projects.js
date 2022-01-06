816e13fee5aa725d1f3d1e2d701666856c7451ed
haelina <56834622+haelina@users.noreply.github.com>
2022-01-06 18:28:49 +0200

    Modify projectcards

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -2,14 +2,34 @@ import React from "react";
 import { ProjectCard } from "../organisms/ProjectCard";
 
 export const Projects = () => {
-  const projects = [{ title: "esim1" }];
+  const projects = [
+    {
+      id: 1,
+      title: "esim1",
+      description: "Lorefggkhojb gojnig gigkjbjf gugkbj ggibkbkgkhgkjbk",
+      img: require("./../../images/app1.jpg").default,
+    },
+    {
+      id: 2,
+      title: "esim2",
+      description: "Lorefggkhojb gojnig gigkjbjf gugkbj ggibkbkgkhgkjbk",
+      img: require("./../../images/app1.jpg").default,
+    },
+  ];
   return (
     <>
       <main>
         <h2>PROJEKTIT</h2>
         <p>That feels like an existential question, don't you think?</p>
         {projects.map((p) => {
-          return <ProjectCard title={p.title}></ProjectCard>;
+          return (
+            <ProjectCard
+              key={p.id}
+              img={p.img}
+              title={p.title}
+              description={p.description}
+            ></ProjectCard>
+          );
         })}
       </main>
     </>
--- src/Components/organisms/ProjectCard.js
+++ src/Components/organisms/ProjectCard.js
@@ -3,7 +3,13 @@ import React from "react";
 export const ProjectCard = ({ title, img, description }) => {
   return (
     <div className="projectcard">
-      <h2>{title}</h2>
+      <div className="leftColumn">
+        <img src={img} alt={title} width="90%"></img>
+      </div>
+      <div className="rightColumn">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
