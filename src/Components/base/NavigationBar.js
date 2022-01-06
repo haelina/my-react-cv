bc42c90ea9936a177572b09e4b151ded18d8dd77
haelina <56834622+haelina@users.noreply.github.com>
2022-01-06 16:50:03 +0200

    Delete unnecessary button component

--- src/Components/base/NavigationBar.js
+++ src/Components/base/NavigationBar.js
@@ -3,17 +3,40 @@ import React, { useState } from "react";
 export const NavigationBar = ({ handleClick }) => {
   const [currentPage, setCurrentPage] = useState("/");
 
+  const changePage = (page) => {
+    handleClick(page);
+    setCurrentPage(page);
+  };
+
   return (
     <div className="ButtonBar">
-      <button className="button_primary" onClick={() => handleClick("/")}>
+      <button
+        className={`button_primary ${
+          currentPage === "/"
+            ? "button_primary__active"
+            : "button_primary__inactive"
+        }`}
+        onClick={() => changePage("/")}
+      >
         Etusivu
       </button>
-      <button className="button_primary" onClick={() => handleClick("/cv")}>
+      <button
+        className={`button_primary ${
+          currentPage === "/cv"
+            ? "button_primary__active"
+            : "button_primary__inactive"
+        }`}
+        onClick={() => changePage("/cv")}
+      >
         CV
       </button>
       <button
-        className="button_primary"
-        onClick={() => handleClick("/projects")}
+        className={`button_primary ${
+          currentPage === "/projects"
+            ? "button_primary__active"
+            : "button_primary__inactive"
+        }`}
+        onClick={() => changePage("/projects")}
       >
         Projects
       </button>
