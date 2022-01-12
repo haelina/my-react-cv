457cc178e5b08245a9a393020acd8ec430812b73
haelina <56834622+haelina@users.noreply.github.com>
2022-01-12 15:53:17 +0200

    Modify introduction text

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -5,8 +5,50 @@ export const Home = () => {
     <>
       <main>
         <h2>Hanna tässä moi!</h2>
-        <p>Tähän tulee infoa minusta.</p>
-        <p>Löytyy projektitiedot ja aikaisempi työhistoriakin.</p>
+        <p>
+          Tervetuloa tutustumaan mun työhistoriaan ja projekteihin, joissa oon
+          ollut mukana. Suurin osa projekteistani pyörii myös Herokussa, joten
+          niiden toimintaa on helppo vilkaista nopeasti.
+        </p>
+        <h4>No kukas tää Hanna on?</h4>
+        <p className="hometext">
+          Mää oon Etelä-Pohjanmaalta lähtöisin oleva maitotilan kasvatti. Vuonna
+          2011 suuntasin Tampereen seudulle ja viihdyn nykyään Kangasalla.
+          <br />
+          <br />
+          Olen kissaihminen, rakastan hidasta arkea ja omassa tahdissa
+          puuhastelua, sekä luonnossa oleilua. Hyvinvointi ja mielekäs tekeminen
+          ovat nousseet viime vuosina elämässäni tärkeiksi teemoiksi, ja
+          rentoudun parhaiten siivoamalla, lukemalla kirjaa tai harrastamalla
+          liikuntaa.
+          <br />
+          <br />
+          Luonteeltani olen rauhallinen ja järjestelmällinen. Olen aina pitänyt
+          matematiikasta ja matematiikka on myös yksi vahvoista puolistani.
+          Tästä on usein apua erilaisten ongelmien ratkaisussa. Asioiden
+          aikataulutuksessa olen erityisen hyvä, sillä tehtävät on mielestäni
+          parempi hoitaa ajallaan ilman suurta stressiä.
+        </p>
+        <h4>Työhistoria?</h4>
+        <p className="hometext">
+          Lapsuus ja nuoruus tuli pyörittyä luonnollisesti maitotilan töissä.
+          Vuosien aikana olen ollut pariin otteeseen kaupan kassahenkilönäkin
+          (Veljekset Keskinen Oy ja Siwa/Valintatalo). Lukion jälkeen perustin
+          naisten alusvaatteisiin erikoistuneen verkkokaupan, jota pyöritin
+          päätoimisesti noin kahdeksan vuoden ajan ja sen jälkeen vielä
+          sivutoimisesti muiden töiden ohessa. Vuonna 2015 elämä vei siivousalan
+          töihin, jotka lopetin vuonna 2019 kun raskaan fyysisen työn tekeminen
+          kävi mahdottomaksi. Alanvaihto tuli ajankohtaiseksi ja 2019 pääsin
+          Tampereen ammattikorkeakouluun opiskelemaan tietojenkäsittelyä.
+        </p>
+        <h4>Entäs tulevaisuus?</h4>
+        <p className="hometext">
+          Toivoisin pääseväni hyvään työpaikkaan mukavien työkaverien
+          ympäröimäksi, jotta voisin kehittää osaamistani seuraavalle tasolle.
+          Etätyömahdollisuus on myös suuri plussa, sillä olen haaveillut
+          paikkariippumattomasta työstä, jota voisin tehdä mistäpäin Suomea
+          vain.
+        </p>
       </main>
     </>
   );
